5b1af8036884a2e1d5d403d557ec1bfbc413c3c9
Henny Borodiansky <sarabo@email.com>
2026-05-13 00:01:19 +0300

    first commit henny

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { WelcomePageComponent } from '../components/welcome-page/welcome-page.component';
+
+export const routes: Routes = [
+     {path: '', component: WelcomePageComponent},
+];
